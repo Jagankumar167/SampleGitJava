90b5f39d343b6dd4b16019529e52f7263c7190b4
Jagankumar167 <chitrajagan1672000@gmail.com>
2022-11-15 14:04:47 +0530

    Done

---
+++ src/org/sample_git/GitJava.java
@@ -0,0 +1,15 @@
+package org.sample_git;
+
+public class GitJava {
+	
+	private void empId() {
+		System.out.println("The Employee Id is---> 253446465 ");
+
+	}
+	public static void main(String[] args) {
+		GitJava gj = new GitJava();
+		gj.empId();
+		
+	}
+
+}
