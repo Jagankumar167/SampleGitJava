524901166408d9e2a19438a1d36d69bce4babd54
Jagankumar167 <chitrajagan1672000@gmail.com>
2022-11-17 14:13:09 +0530

    Jagan finsih the work

--- src/org/sample_git/GitJava.java
+++ src/org/sample_git/GitJava.java
@@ -6,6 +6,10 @@ public class GitJava {
 		System.out.println("The Employee Id is---> 253446465 ");
 
 	}
+	private void empName() {
+		System.out.println("The Employee Name is----> Jagankumar");
+
+	}
 	public static void main(String[] args) {
 		GitJava gj = new GitJava();
 		gj.empId();
