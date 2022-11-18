788f061c6e3bff9ba6fac15d9089c39c56d30e0c
Jagankumar167 <chitrajagan1672000@gmail.com>
2022-11-18 13:00:47 +0530

    Sowndharya Merged

--- src/org/sample_git/GitJava.java
+++ src/org/sample_git/GitJava.java
@@ -4,6 +4,11 @@ public class GitJava {
 	
 	private void empId() {
 		System.out.println("The Employee Id is---> 253446465 ");
+		
+
+	}
+	private void empSalary() {
+		System.out.println("The Employee Salary is---> 20,000");
 
 	}
 	public static void main(String[] args) {
